589e55b7dc240a5cd7f757d081e70302d2d1e74f
aymen <aymen.sellaouti@gmail.com>
2020-02-08 12:59:15 +0100

    support de cours

--- assets/js/innerHtml.js
+++ assets/js/innerHtml.js
@@ -1,2 +1,15 @@
 
 villes = ['Tunis', 'Sousse', 'Sfax', 'Djerba'];
+
+let listeVilles = document.querySelector('#listeVilles');
+i = 0;
+
+indiceInterval = setInterval(
+    function () {
+        listeVilles.innerHTML+= `<li>${villes[i]}</li>`;
+        i++;
+        if(i === villes.length) {
+            clearInterval(indiceInterval);
+        }
+    }, 2000
+);
